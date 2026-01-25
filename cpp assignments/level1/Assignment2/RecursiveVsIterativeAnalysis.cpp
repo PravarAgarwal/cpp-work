ca8bd80d27148e0893e552ff8f1d6c658db1a857
PravarAgarwal <pravar20229@iiitd.ac.in>
2026-01-25 22:17:17 +0530

    fibonacci functions added

--- cpp assignments/level1/Assignment2/RecursiveVsIterativeAnalysis.cpp
+++ cpp assignments/level1/Assignment2/RecursiveVsIterativeAnalysis.cpp
@@ -3,10 +3,15 @@
 int factorial_recursive(int n);
 int factorial_iterative(int n);
 
+int fibonacci_recursive(int n);
+int fibonacci_iterative(int n);
+
 int main()
 {
     std::cout << "Factorial of 5 (Recursive): " << factorial_recursive(5) << std::endl;
     std::cout << "Factorial of 5 (Iterative): " << factorial_iterative(5) << std::endl;
+    std::cout << "Fibonacci of 5 (Recursive): " << fibonacci_recursive(10) << std::endl;
+    std::cout << "Fibonacci of 5 (Recursive): " << fibonacci_iterative(10) << std::endl;
     return 0;
 }
 
@@ -30,6 +35,38 @@ int factorial_iterative(int n)
     for (int i = 1; i < n; i++)
     {
         ans *= (i + 1);
+    }
+
+    return ans;
+}
+
+int fibonacci_recursive(int n)
+{
+    // base case
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    // sp
+    int sp1 = fibonacci_recursive(n - 1);
+    int sp2 = fibonacci_recursive(n - 2);
+    return sp1 + sp2;
+}
+
+int fibonacci_iterative(int n)
+{
+    int ans = 0;
+    int a = 0, b = 1;
+
+    if (n <= 1)
+        return n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        ans = a + b;
+        a = b;
+        b = ans;
     }
 
     return ans;
